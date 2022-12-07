9560a4da1f163916a29d607835eb3283a49a861a
avnva <voynova.03@inbox.ru>
2022-12-07 20:04:42 +0700

    Add generation bricks and levels

--- Arkanoid/Assets/Scripts/Brick.cs
+++ Arkanoid/Assets/Scripts/Brick.cs
@@ -10,10 +10,15 @@ public class Brick : MonoBehaviour
 
     public static event Action<Brick> OnBrickDestruction;
 
-    private void Start()
+    //private void Start()
+    //{
+    //    this.sr = this.GetComponent<SpriteRenderer>();
+    //    this.sr.sprite = BricksManager.Instance.Sprites[this.Hitpoints - 1];
+    //}
+
+    private void Awake()
     {
         this.sr = this.GetComponent<SpriteRenderer>();
-        this.sr.sprite = BricksManager.Instance.Sprites[this.Hitpoints - 1];
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,5 +38,13 @@ public class Brick : MonoBehaviour
         {
             this.sr.sprite = BricksManager.Instance.Sprites[this.Hitpoints - 1];
         }
+    }
+
+    public void Init(Transform containerTransform, Sprite sprite, Color color, int hitpoints)
+    {
+        this.transform.SetParent(containerTransform);
+        this.sr.sprite = sprite;
+        this.sr.color = color;
+        this.Hitpoints = hitpoints;
     }
 }
--- Arkanoid/Assets/Scripts/BricksManager.cs
+++ Arkanoid/Assets/Scripts/BricksManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BricksManager : MonoBehaviour
@@ -22,5 +24,106 @@ public class BricksManager : MonoBehaviour
 
     #endregion
 
+    private int maxRows = 17;
+    private int maxCols = 12;
+    private GameObject bricksContainer;
+    //начальное положение первого кирпича
+    private float initialBrickSpawnPositionX = -7.8f;
+    private float initialBrickSpawnPositionY = 3.24f;
+    private float shiftAmount = 1.001f;
+
+    public Brick brickPrefab;
+
     public Sprite[] Sprites;
+
+    public Color[] BrickColors;
+
+    public List<Brick> RemainingBricks { get; set; }
+
+    public List<int[,]> LevelsData { get; set; }
+
+    public int InitialBricksCount { get; set; }
+
+    public int CurrentLevel;
+
+    private void Start()
+    {
+        this.bricksContainer = new GameObject("BricksContainer");
+        this.RemainingBricks = new List<Brick>();
+        this.LevelsData = this.LoadLevelsData();
+        this.GenerateBricks();
+    }
+
+    private void GenerateBricks()
+    {
+        int[,] currentLevelData = this.LevelsData[this.CurrentLevel];
+        float currentSpawnX = initialBrickSpawnPositionX;
+        float currentSpawnY = initialBrickSpawnPositionY;
+        //чтобы кирпичи не перекрывались
+        float zShift = 0;
+
+        for (int row = 0; row <this.maxRows; row++)
+        {
+            for (int col = 0; col < this.maxCols; col++)
+            {
+                int brickType = currentLevelData[row, col];
+
+                if (brickType > 0)
+                {
+                    Brick newBrick = Instantiate(brickPrefab, new Vector3(currentSpawnX, currentSpawnY, 0.0f - zShift), Quaternion.identity) as Brick;
+                    newBrick.Init(bricksContainer.transform, this.Sprites[brickType - 1], this.BrickColors[brickType], brickType);
+                    
+                    this.RemainingBricks.Add(newBrick);
+                    zShift += 0.0001f;
+                }
+
+                currentSpawnX += shiftAmount;
+                //достигли ли конца столбцов
+                if (col + 1 == this.maxCols)
+                {
+                    currentSpawnX = initialBrickSpawnPositionX;
+                }
+            }
+
+            currentSpawnY -= shiftAmount;
+        }
+
+        this.InitialBricksCount = this.RemainingBricks.Count;
+    }
+
+    private List<int[,]> LoadLevelsData()
+    {
+        TextAsset text = Resources.Load("levels") as TextAsset;
+
+        string[] rows = text.text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+        List<int[,]> levelsData = new List<int[,]>();
+        int[,] currentLevel = new int[maxRows, maxCols];
+        int currentRow = 0;
+
+        for (int row = 0; row <rows.Length; row++)
+        {
+            string line = rows[row];
+
+            if (line.IndexOf("--") == -1)
+            {
+                string[] bricks = line.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < bricks.Length; col++)
+                {
+                    currentLevel[currentRow, col] = int.Parse(bricks[col]);
+                }
+
+                currentRow++;
+            }
+            else
+            {
+                //end of current level
+                currentRow = 0;
+                levelsData.Add(currentLevel);
+                currentLevel = new int[maxRows, maxCols];
+            }
+        }
+
+        return levelsData;
+    }
 }
